300e949a0adc163504370abec6b3304beb943b47
Des4037 <Des4037>
2023-05-10 16:34:02 +0930

    1

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using namespace std;
+
+class Person {
+private:
+    string name;
+    int ID;
+    int age;
+    float weight;
+public:
+    Person(string name, int ID, int age, float weight);
+    Person();
+    void setName(string name);
+    string getName();
+    void setID(int ID);
+    int getID();
+    void setAge(int age);
+    int getAge();
+    void setWeight(float weight);
+    float getWeight();
+};
+
+#endif /* PERSON_H */
