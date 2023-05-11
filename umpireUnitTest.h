5974043f24b112b05d2b2669aaeabee0ccb2e689
jp2920 <a182386@adelaide.edu.au>
2023-05-11 13:32:21 +0930

    commit

---
+++ umpireUnitTest.h
@@ -0,0 +1,14 @@
+#ifndef UMPIREUNITTEST_H
+#define UMPIREUNITTEST_H
+
+#include <iostream>
+
+class UmpireUnitTest {
+    private: 
+        void testUmpire
+    public: 
+        void runTests() { 
+            testUmpire(); 
+        }           
+
+}
