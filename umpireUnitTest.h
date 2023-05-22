01279abf12f71bef7bee7f6dbd272a0a99495ea7
jp2920 <a182386@adelaide.edu.au>
2023-05-23 05:55:26 +0930

    commit

--- umpireUnitTest.h
+++ umpireUnitTest.h
@@ -2,15 +2,34 @@
 #define UMPIREUNITTEST_H
 
 #include "Umpire.h"
+#include "Person.h"
+#include "Player.h"
+#include "Server.h"
+#include "Receiver.h"
+
 #include <iostream>
 
-class UmpireUnitTest : public Umpire{
+using namespace std; 
+
+class UmpireUnitTest {
     public: 
-        void runTests() { 
+        void runUmpireTests() { 
+            /* Tests for functions of UMPIRE */
+                testUmpire(); 
+
+
         }      
     
     private: 
-        void test () {} ;     
+    /* Umpire test functions */
+        void testUmpire() {
+            Umpire umpire; 
+            Server server;
+            // serving function
+            if (umpire.serve(server) != 0) {
+                cout << "test failed" << endl;
+            }
+        }    
 
 };
 
