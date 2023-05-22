dd3ef32a116b399109b1a93091a5f15d0a627754
jp2920 <a182386@adelaide.edu.au>
2023-05-23 03:27:38 +0930

    added input from the user to the main function

--- README.md
+++ README.md
@@ -4,8 +4,32 @@
 ## Our Simulation: 
 This simulation is between two tennis players that can be inputted by the user.
 The idea is to determine who would be the best tennis player out of the two simulated players
+
+
 ## Instructions: 
-1.  Enter the name and the identification number of the player the enter name should be a string and an integer
-2. This will then calculate the who will win out of the two players
+1. Enter the name of the player and the stats of the player that are prompted.
+2. This will then calculate the winner of each point and display the winner out of the two players.
+3. 
+
+
+## Stats you will need to enter
+Name: This is the player's full name.
+
+Age: The age of the player in years.
+
+Weight: The mass of the player in kg.
+
+Power: The power has to be between 1 and 10 (1-10).
+
+Speed: How fast the player moves on the court out of 100 (1-10).
+
+Rallying ability: How well out of 10 the player can rally (1 to 10).
+
+Receiving ability: How well the receiver can return a serve out of 10 (1 to 10).
+
+Serving ability: How well the server can serve to the opponent out of 10 (1 to 10).
+
+
+
 
 
--- main.cpp
+++ main.cpp
@@ -15,6 +15,157 @@
 using namespace std;
 
 int main(){
+
+    // Read in the read me file
+    
+
+    //Seed the random number generator using the current time
+    srand(time(nullptr));
+    
+    Umpire umpire; 
+
+    // Server input
+    Server server;
+
+    cout << "What is the Server's name?\n";  
+    string name ; 
+    cin >> name;
+    server.setName(name);
+
+    //enter the age
+    cout << "What is the server's Age\n";
+    int age;
+    cin >> age;
+    server.setAge(age);
+
+    // enter weight
+    cout << "What is the Server's weight\n"; 
+    int weight;
+    cin >> weight;
+    server.setWeight(weight);
+
+    // enter power
+    cout << "What is the Server's power\n"; 
+    int power;
+    cin >> power;
+    server.setPower(power);
+
+    // enter speed
+    cout << "What is the Server's speed\n"; 
+    int speed;
+    cin >> speed;
+    server.setRawSpeed(speed);
+
+    // enter rallying ability
+    cout << "What is the Server's rallying ability\n"; 
+    int rallyingAbility;
+    cin >> rallyingAbility;
+    server.setRallyingAbility(rallyingAbility);
+
+    // enter serving ability
+    cout << "What is the Server's serving ability\n"; 
+    int servingAbility;
+    cin >> servingAbility;   
+    server.setServingAbility(servingAbility);
+
+    server.setLuck();
+    
+    this_thread::sleep_for(chrono::milliseconds(150));
+
+
+
+
+    /* Receiver input */
+    Receiver receiver;
+
+
+    cout << "What is the receiver's name?\n";  
+    string nameReceiver; 
+    cin >> nameReceiver;
+    receiver.setName(nameReceiver);
+
+    //enter the age
+    cout << "What is the receiver's Age\n";
+    int ageReceiver;
+    cin >> ageReceiver;
+    receiver.setAge(ageReceiver);
+
+    // enter weight
+    cout << "What is the receiver's weight\n"; 
+    int weightReceiver;
+    cin >> weightReceiver;
+    receiver.setWeight(weightReceiver);
+
+    // enter power
+    cout << "What is the receiver's power\n"; 
+    int powerReceiver;
+    cin >> powerReceiver;
+    receiver.setPower(powerReceiver);
+
+    // enter speed
+    cout << "What is the receiver's speed\n"; 
+    int speedReceiver;
+    cin >> speedReceiver;
+    receiver.setRawSpeed(speedReceiver);
+
+    // enter rallying ability
+    cout << "What is the receiver's rallying ability\n"; 
+    int rallyingAbilityReceiver;
+    cin >> rallyingAbilityReceiver;
+    receiver.setRallyingAbility(rallyingAbilityReceiver);
+
+    // enter receiving ability
+    cout << "What is the receiver's receiving ability\n"; 
+    int receivingAbility;
+    cin >> receivingAbility;   
+    receiver.setReceivingAbility(receivingAbility);
+
+    receiver.setLuck();
+   
+    // Starting score for the game
+    cout << "The Server's score is: " << server.getRoundScore() << endl;
+    cout << "The Server's score is: " << receiver.getRoundScore() << endl;
+
+    int x = 1;
+
+    while (x == 1) {
+        umpire.serve(server);
+
+        this_thread::sleep_for(chrono::milliseconds(800));
+        
+        umpire.receive(receiver);
+
+        umpire.givePoints(server, receiver);
+        x = umpire.checkWinner(server, receiver);
+        umpire.announcePoints();
+    }
+    
+    cout << "Thanks for playing!" << endl;
+
+
+    return 0;
+}
+
+
+/*
+
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+
+#include "Printable.h"
+#include "Person.h"
+#include "Player.h"
+#include "Server.h"
+#include "Receiver.h"
+#include "Umpire.h"
+
+using namespace std;
+
+int main(){
     //Seed the random number generator using the current time
     srand(time(nullptr));
     
@@ -73,4 +224,4 @@ int main(){
 
 
     return 0;
-}
+} */
