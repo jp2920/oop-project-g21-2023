c19c679c176bb49498808df276b4acd9384b55a7
jp2920 <a182386@adelaide.edu.au>
2023-05-23 23:56:59 +0930

    done

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ using namespace std;
 
 int main(){
 
-    /*/ Read in the read me file
+    // Read in the read me file
 
 
     // path to the file
@@ -40,7 +40,7 @@ int main(){
     } else {
         cout << "Failure to open the file" << endl;
     }
-    */
+    
 
     //Seed the random number generator using the current time
     srand(time(nullptr));
@@ -169,60 +169,3 @@ int main(){
     return 0;
 }
 
-
-
-
-
-
-/*
-
-int main(){
-    //Seed the random number generator using the current time
-    srand(time(nullptr));
-    
-    Umpire U; 
-
-    Server Jonty;
-    Jonty.setName("Jonty");
-    Jonty.setAge(25);
-    Jonty.setWeight(80);
-    Jonty.setPower(8);
-    Jonty.setRawSpeed(800);
-    Jonty.setRallyingAbility(7);
-    Jonty.setServingAbility(10);
-    Jonty.setLuck();
-    
-    this_thread::sleep_for(chrono::milliseconds(150));
-
-    Receiver Des;
-    Des.setName("Des");
-    Des.setAge(20);
-    Des.setWeight(65);
-    Des.setPower(8);
-    Des.setRawSpeed(700);
-    Des.setRallyingAbility(9);
-    Des.setReceivingAbility(8);
-    Des.setLuck();
-   
-    cout << Jonty.getRoundScore() << endl;
-    cout << Des.getRoundScore() << endl;
-
-    int x = 1;
-    while (x == 1){
-        U.serve(Jonty);
-        this_thread::sleep_for(chrono::milliseconds(800));
-        U.receive(Des);
-
-        U.givePoints(Jonty, Des);
-        x = U.checkWinner(Jonty, Des);
-        U.announcePoints();
-    }
-    
-    cout << "Thanks for playing!" << endl;
-
-
-    return 0;
-}
-
-
-*/
